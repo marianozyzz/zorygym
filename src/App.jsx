eee8d14ea890f9aa3e30a6c636378f3716f2872f
marianozyzz <marianozyzz@gmail.com>
2026-05-01 13:05:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import HeroSection from './sections/HeroSection'
-import ProblemSection from './sections/ProblemSection'
-import ContentSection from './sections/ContentSection'
-import OfferSection from './sections/OfferSection'
-import Footer from './sections/Footer'
+import HeroSection from "./sections/HeroSection"
+import ProblemSection from "./sections/ProblemSection"
+import ContentSection from "./sections/ContentSection"
+import OfferSection from "./sections/OfferSection"
 
 export default function App() {
   return (
-    <main className="overflow-hidden">
+    <main>
       <HeroSection />
       <ProblemSection />
       <ContentSection />
       <OfferSection />
-      <Footer />
     </main>
   )
 }
